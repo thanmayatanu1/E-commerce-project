dc14048585bc5ff80501cf1eebcd1795c38d6c83
thanmayatanu1 <thanmayatanu1@gmail.com>
2023-07-30 10:51:20 +0530

    Auto logout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { useContext} from 'react';
+import { useContext } from 'react';
 import './App.css';
 import AuthContext from './components/Authentication/AuthContext';
 import Header from './components/Navbar/Header';
 import Product from './components/Store/Product';
 import CartList from './components/Store/CartList';
-import { BrowserRouter as Router, Routes, Route,  } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import About from './components/About/About';
 import Home from './components/Home/Home';
 import FetchMovies from './components/Movies/FetchMovies';
@@ -14,38 +14,32 @@ import { ProductList, useCart, handleFormSubmit} from './components/Store/Produc
 import AuthForm from './components/Authentication/AuthForm';
 import UserProfile from './components/Navbar/UserProfile';
 
-
-
 function App() {
   const authCtx = useContext(AuthContext);
   const { cart, showCart, addToCart, handleShow } = useCart();
-  
-  
 
+  
+  
   return (
     <Router>
       <Header count={cart.length} handleShow={handleShow} />
       <Routes>
-          <Route path="/" element={<Product product={ProductList} addToCart={addToCart} />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact us" element={<ContactUs handleFormSubmit={handleFormSubmit} />} />
-          <Route path="/fetchmovies" element={<FetchMovies handleFormSubmit={handleFormSubmit}/>} />
-          <Route path="/product/:productId" element={<ProductPage product={ProductList} />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/cart" element={showCart && <CartList key="cart" cart={cart} />} />
-          {!authCtx.isLoggedIn && (
-          <Route path="/auth" 
-          element={<AuthForm />} 
-          />
-          )}
-          {authCtx.isLoggedIn && (
-          <Route path="/profile" 
-          element={<UserProfile />} 
-          />
-          )}
-          
-        </Routes>
-      </Router>
+        <Route path="/" element={<Product product={ProductList} addToCart={addToCart} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact us" element={<ContactUs handleFormSubmit={handleFormSubmit} />} />
+        <Route path="/fetchmovies" element={<FetchMovies handleFormSubmit={handleFormSubmit}/>} />
+        <Route path="/product/:productId" element={<ProductPage product={ProductList} />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/cart" element={showCart && <CartList key="cart" cart={cart} />} />
+        {!authCtx.isLoggedIn && (
+          <Route path="/auth" element={<AuthForm />} />
+        )}
+        {authCtx.isLoggedIn && (
+          <Route path="/profile" element={<UserProfile />} />
+        )}
+       
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Authentication/AuthContext.js
+++ src/components/Authentication/AuthContext.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const AuthContext = React.createContext({
   token: '',
@@ -16,12 +16,41 @@ export const AuthContextProvider = (props) => {
   const loginHandler = (token) => {
     setToken(token);
     localStorage.setItem('token', token);
+    startLogoutTimer();
   };
 
   const logoutHandler = () => {
     setToken(null);
     localStorage.removeItem('token');
   };
+
+  const startLogoutTimer = () => {
+    const expirationTime = 1000 * 60 * 1000; // 5 minutes in milliseconds
+
+    const logoutTimer = setTimeout(() => {
+      logoutHandler();
+      alert('Your session has expired. Please log in again.');
+    }, expirationTime);
+
+    // Clear the timer when the user manually logs out or closes the browser
+    return () => clearTimeout(logoutTimer);
+  };
+
+  useEffect(() => {
+    // Check if there is a token and if it is still valid
+    if (initialToken && userIsLoggedIn) {
+      const expirationTime = localStorage.getItem('expirationTime');
+      const currentTime = new Date().getTime();
+
+      if (currentTime >= expirationTime) {
+        logoutHandler();
+        alert('Your session has expired. Please log in again.');
+      } else {
+        const remainingTime = expirationTime - currentTime;
+        startLogoutTimer(remainingTime);
+      }
+    }
+  }, [initialToken, userIsLoggedIn]);
 
   const contextValue = {
     token: token,
