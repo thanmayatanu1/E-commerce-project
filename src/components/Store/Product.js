8cba1738a12ec39b8aac6e689f97a78f0566a98e
thanmayatanu1 <thanmayatanu1@gmail.com>
2023-08-01 12:03:30 +0530

    updated

--- src/components/Store/Product.js
+++ src/components/Store/Product.js
@@ -1,24 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import classes from './ProductList.module.css';
 
 function Product({ product, addToCart }) {
+  const musicProducts = product.slice(0, 4); // First four products
+  const mergeProducts = product.slice(4, 6); // Last two products
+
   return (
-    <div className='product'>
-      {product.map((productItem) => (
-        <div key={productItem.id} style={{ width: '50%' }}>
-          <div className='product-item'>
-            <div>
-              <Link to={`/product/${productItem.id}`}>
-                <img src={productItem.imageUrl} width="300px" alt={productItem.title} />
-              </Link>
-              <h2>{productItem.title}</h2>
-              <p>Price: Rs. {productItem.price}</p>
-              <button onClick={() => addToCart(productItem)}>Add To Cart</button>
+    <div>
+      <div>
+        <header className={classes.sectionTitle}>Music</header>
+        <div className={classes.product}>
+          {musicProducts.map((productItem) => (
+            <div key={productItem.id} className={classes.productitem}>
+              <div>
+                <Link to={`/product/${productItem.id}`}>
+                  <img src={productItem.imageUrl} width="300px" alt={productItem.title} />
+                </Link>
+                <h2>{productItem.title}</h2>
+                <p>Price: Rs. {productItem.price}</p>
+                <button onClick={() => addToCart(productItem)}>Add To Cart</button>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
-      ))}
-    </div>
+      </div>
+      <div>
+        <header className={classes.sectionTitle}>Merge</header>
+        <div className={classes.product}>
+          {mergeProducts.map((productItem) => (
+            <div key={productItem.id} className={classes.productitem}>
+              <div>
+                <Link to={`/product/${productItem.id}`}>
+                  <img src={productItem.imageUrl} width="300px" alt={productItem.title} />
+                </Link>
+                <h2>{productItem.title}</h2>
+                <p>Price: Rs. {productItem.price}</p>
+                <button onClick={() => addToCart(productItem)}>Add To Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+  </div>
   );
 }
 
